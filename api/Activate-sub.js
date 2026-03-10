110b4d8ad2833cca8aa1ab4f3fd448c121b16a14
ahmedfah610-star <ahmedfah610@gmail.com>
2026-03-10 11:32:35 +0100

    Update Activate-sub.js

--- api/Activate-sub.js
+++ api/Activate-sub.js
@@ -1,4 +1,4 @@
-import { initializeApp, cert, getApps } from 'firebase-admin/app'
+import { initializeApp, cert, getApps } from 'firebase-admin/app';
 import { getFirestore, Timestamp } from 'firebase-admin/firestore';
 import { getAuth } from 'firebase-admin/auth';
 
@@ -22,6 +22,7 @@ export default async function handler(req, res) {
   });
   return res.status(200).json({ ok: true, expiresAt: expiresAt.toISOString() });
 }
+
 
 
 
